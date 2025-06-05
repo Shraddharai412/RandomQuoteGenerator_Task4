1c7245cb42f569417782ae7e32aa13168df09692
Shraddharai412 <shraddharai412@gmail.com>
2025-06-05 23:08:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import RandomQuoteGenerator from "./Components/quotegenerator";
+function App()
+{
+  return(
+    <div style={{marginTop:'10%'}}>
+   <RandomQuoteGenerator/>
+  </div>
+  )
+}
+export default App;
